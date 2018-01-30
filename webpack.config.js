72379112708e7195068d1f242736725d888438ae
Adam Więcek <adamw28@gmail.com>
2018-01-30 21:53:23 +0100

    Does not work React

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 const path = require('path');
+var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 plugins: [new HtmlWebpackPlugin({
-  template: 'src/index.html',
-  filename: 'index.html',
-  inject: 'body'
-})]
+    template: 'src/index.html',
+    filename: 'index.html',
+    inject: 'body'
+    }),
+    new webpack.optimize.UglifyJsPlugin()]
 
 module.exports = {
     entry: './src/index.js',
