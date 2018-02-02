84de149081d4509db1177adc4ed2998d22d6ba1d
Adam Więcek <adamw28@gmail.com>
2018-02-02 01:35:42 +0100

    Update project

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,146 +2,95 @@ import React from 'react';
 import style from './App.css';
 import uuid from 'uuid';
 
-        class TodoList extends React.Component {
-            constructor(props) {
-                super(props);
-                this.state = {
-                    todos: [{
-                        id: 1,
-                        text: 'clean room'
-                    }, {
-                        id: 2,
-                        text: 'wash the dishes'
-                    }, {
-                        id: 3,
-                        text: 'feed my cat'
-                    }],
-                    currentText: ''
-                };
-            }
-            addTodo() {
-                const todo = {
-                    text: this.state.currentText,
-                    id: uuid.v4()
-                }
+const TodoList = props => {
+    return ( <
 
-                this.setState({
-                    todos: [...this.state.todos, todo]
-                });
-            }
-            removeTodo(id) {
-                const reminder = this.state.todos.filter(todo => todo.id !== id);
-
-                this.setState({
-                    todos: reminder
-                });
-            }
-
-            handlePress(ev) {
-                this.setState({
-                    currentText: ev.target.value
-                });
-
-            }
-
-            renderTodo(todo) {
-                return ( < li key = { todo.id } > { todo.text } < button type = "button"
-                    onClick = {
-                        () => { this.removeTodo(todo.id) }
-                    } > Usuń < /button></li > )
-            }
-
-            render() {
-                return ( <
-                    div >
-                    <
-                    form className = { style.TodoApp } onSubmit = {
-                        (event) => { this.addTodo }
-                    } >
-                    <
-                    h1 > Lista rzeczy do zrobienia < /h1> <
-                    input type = "text"
-                    id = "title"
-                    onChange = { this.handlePress.bind(this) }
-                    /> <
-                    button type = "button"
-                    onClick = { this.addTodo.bind(this) } > Add title < /button> < /
-                    form >
-                    <h1>Liczba zadań: {this.state.todos.length}</h1>
-                    <
-                    ul > { this.state.todos.map(todo => this.renderTodo(todo)) } <
-                    /ul> < /
-                    div >
-
-                );
-            }
+        ul >
+        todos = {
+            props.state.todos
         }
-        /*
-                class App extends React.Component {
-                    constructor(props) {
-                        super(props);
-                        this.state = {
-                            todos: [],
-                            currentText: ''
-                        };
-                    }
-                    addTodo() {
-                        const todo = {
-                            text: this.state.currentText,
-                            id: uuid.v4()
-                        }
+        remove = {
+            props.removeTodo
+        } { props.state.todos.map(todo => this.renderTodo(props.todo)) } <
+        /ul>
 
-                        this.setState({
-                            todos: [...this.state.todos, todo]
-                        });
-                    }
-                    removeTodo(id) {
-                        const reminder = this.state.todos.filter(todo => todo.id !== id);
+    );
 
-                        this.setState({
-                            todos: reminder
-                        });
-                    }
+}
 
-                    handlePress(ev) {
-                        this.setState({
-                            currentText: ev.target.value
-                        });
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            todos: [{
+                id: 1,
+                text: 'clean room'
+            }, {
+                id: 2,
+                text: 'wash the dishes'
+            }, {
+                id: 3,
+                text: 'feed my cat'
+            }],
+            currentText: ''
+        };
+    }
+    addTodo() {
+        const todo = {
+            text: this.state.currentText,
+            id: uuid.v4()
+        }
 
-                    }
+        this.setState({
+            todos: [...this.state.todos, todo]
+        });
+    }
+    removeTodo(id) {
+        const reminder = this.state.todos.filter(todo => todo.id !== id);
 
-                    renderTodo(todo) {
-                        return ( < li key = { todo.id } > { todo.text } < button type = "button"
-                            onClick = {
-                                () => { this.removeTodo(todo.id) }
-                            } > Usuń < /button></li > )
-                    }
+        this.setState({
+            todos: reminder
+        });
+    }
 
-                    render() {
-                        return ( <
-                            div >
-                            <
-                            form className = { style.TodoApp } onSubmit = {
-                                (event) => { this.addTodo }
-                            } >
-                            <
-                            h1 > Lista rzeczy do zrobienia < /h1> <
-                            input type = "text"
-                            id = "title"
-                            onChange = { this.handlePress.bind(this) }
-                            /> <
-                            button type = "button"
-                            onClick = { this.addTodo.bind(this) } > Add title < /button> < /
-                            form >
+    handlePress(ev) {
+        this.setState({
+            currentText: ev.target.value
+        });
 
-                            <
-                            ul > { this.state.todos.map(todo => this.renderTodo(todo)) } <
-                            /ul> < /
-                            div >
+    }
 
-                        );
-                    }
-                }
+    renderTodo(todo) {
+        return ( < li key = { todo.id } > { todo.text } < button type = "button"
+            onClick = {
+                () => { this.removeTodo(todo.id) }
+            } > Usuń < /button></li > )
+    }
 
-                export default App;*/
-        export default TodoList;
+    render() {
+        return ( <
+            div >
+            <
+            form className = { style.TodoApp } onSubmit = {
+                (event) => { this.addTodo }
+            } >
+            <
+            h1 > Lista rzeczy do zrobienia < /h1> <
+            input type = "text"
+            id = "title"
+            onChange = { this.handlePress.bind(this) }
+            /> <
+            button type = "button"
+            onClick = { this.addTodo.bind(this) } > Add title < /button> < /
+            form >
+            <
+            h1 > Liczba zadań: { this.state.todos.length } < /h1> <
+            ul > { this.state.todos.map(todo => this.renderTodo(todo)) } <
+            /ul> < /
+            div >
+
+        );
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import TodoList from './containers/App';
+import App from './containers/App';
 
 ReactDOM.render(
-    <TodoList />,
+    <App />,
     document.getElementById('app')
 );
